3b6f834143c9b1fb66ee4bcb152afe14321ddafa
Joep Schuurkes <j19sch@gmail.com>
2023-10-14 16:33:13 +0200

    adds test for puzzle 3

--- tests/puzzle-tests/test_puzzle003.lua
+++ tests/puzzle-tests/test_puzzle003.lua
@@ -3,6 +3,8 @@ import "../source/puzzles/puzzle003"
 TestPuzzle003 = {}
 
 
+-- ToDo: figure out if I can make the tests better describe the puzzle
+
 function TestPuzzle003:testPuzzle3UndockCrank()
 	local puzzle003 = Puzzle003:new()
 	luaunit.assertEquals(puzzle003.elements.toggle.flipped, false)
@@ -10,12 +12,24 @@ function TestPuzzle003:testPuzzle3UndockCrank()
 	luaunit.assertEquals(puzzle003.elements.toggle.flipped, true)
 end
 
-function TestPuzzle003:testPuzzle3DockCrank()
+function TestPuzzle003:testPuzzle3DockCrankFilled()
 	local puzzle003 = Puzzle003:new()
 	puzzle003.elements.toggle.flipped = true
+	puzzle003.elements.circle.fill = true
 	puzzle003:logic(true, 0)
 	luaunit.assertEquals(puzzle003.elements.toggle.flipped, false)
 	luaunit.assertEquals(puzzle003.elements.circle.r, 10)
+	luaunit.assertEquals(puzzle003.elements.circle.fill, true)
+end
+
+function TestPuzzle003:testPuzzle3DockCrankLine()
+	local puzzle003 = Puzzle003:new()
+	puzzle003.elements.toggle.flipped = true
+	puzzle003.elements.circle.fill = false
+	puzzle003:logic(true, 0)
+	luaunit.assertEquals(puzzle003.elements.toggle.flipped, false)
+	luaunit.assertEquals(puzzle003.elements.circle.r, 10)
+	luaunit.assertEquals(puzzle003.elements.circle.fill, false)
 end
 
 function TestPuzzle003:testPuzzle3RotateCrankPos()

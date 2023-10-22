0a1440ff083eacf5dcc90f5bf23b92d86e27af96
Joep Schuurkes <j19sch@gmail.com>
2023-10-22 17:07:35 +0200

    puzzle004: connects button to blinker with adjustable duration

--- source/puzzles/puzzle004.lua
+++ source/puzzles/puzzle004.lua
@@ -8,6 +8,10 @@ import "../elements/button"  -- should use a light instead
 Puzzle004 = {}
 
 function Puzzle004:new()
+	local blinker = gfx.animation.blinker.new(1000, 1000, true, 6, true)
+	-- cycle needs to be an even number to avoid a glitch false
+	-- counter counts each true and false
+	blinker:startLoop()
     local newObj = {
 		elements = {
 			button_1 = Button:new(25, 92, false),
@@ -16,7 +20,8 @@ function Puzzle004:new()
 			button_4 = Button:new(235, 92, false),
 			button_5 = Button:new(305, 92, false)
 		},
-		blinker = gfx.animation.blinker.new(500, 500, true, 5, true),
+		-- blinker = gfx.animation.blinker.new(500, 500, true, 5, true),
+		blinker = blinker
     }
     self.__index = self
     return setmetatable(newObj, self)
@@ -29,10 +34,33 @@ function Puzzle004:draw()
 	end
 end
 
--- function Puzzle004:logic()
--- end
+function Puzzle004:logic()
+	print("blinker " .. "counter: " .. tostring(self.blinker.counter) .. " on: " .. tostring(self.blinker.on))
+	print("blinker " .. "onDuration: " .. tostring(self.blinker.onDuration) ..
+		" offDuration: " .. tostring(self.blinker.offDuration))
+
+	if self.blinker.on == true and self.elements.button_1.pressed == false then
+		self.elements.button_1:toggle()
+	end
+	if self.blinker.on == false and self.elements.button_1.pressed == true then
+		self.elements.button_1:toggle()
+	end
+
+	self.blinker:update()
+end
 
 
 function Puzzle004:run()
+	if playdate.buttonJustPressed( playdate.kButtonUp ) then
+		print("button up")
+		self.blinker.onDuration = self.blinker.onDuration + 250
+		self.blinker.offDuration = self.blinker.onDuration + 250
+	end
+	if playdate.buttonJustPressed( playdate.kButtonDown ) then
+		print("button down")
+		self.blinker.onDuration = self.blinker.onDuration - 250
+		self.blinker.offDuration = self.blinker.onDuration - 250
+	end
+	self:logic()
 	self:draw()
 end

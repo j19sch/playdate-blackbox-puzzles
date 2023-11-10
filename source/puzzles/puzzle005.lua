1063f0cc8e6aa383d91ff7fb918e7afd0a78b145
Joep Schuurkes <j19sch@gmail.com>
2023-11-10 10:01:08 +0100

    improves puzzle005 limits

--- source/puzzles/puzzle005.lua
+++ source/puzzles/puzzle005.lua
@@ -16,7 +16,7 @@ Puzzle005 = {}
 function Puzzle005:new()
     local newObj = {
 		elements = {
-			dial_left = RoundDial:new(60, 95, 195, true),
+			dial_left = RoundDial:new(60, 95, 180, true),
 			button_left = Button:new(28, 145, true),
 			dial_right = SquareDial:new(340, 95, 0, false),
 			button_right = Button:new(308, 145, false),
@@ -49,38 +49,35 @@ function Puzzle005:run()
 		self.elements.dial_left:toggle()
 	end
 
-	if self.elements.dial_left.angle >= 185 and self.elements.dial_left.angle <= 200 and
+	if self.elements.dial_left.angle >= 170 and self.elements.dial_left.angle <= 190 and
 		self.elements.light_1.on == false then
 		self.elements.light_1:toggle()
 	end
 
-	if (self.elements.dial_left.angle < 185 or self.elements.dial_left.angle > 200) and
+	if (self.elements.dial_left.angle < 170 or self.elements.dial_left.angle > 190) and
 		self.elements.light_1.on == true then
 		self.elements.light_1:toggle()
 	end
 
-	if self.elements.dial_right.angle >= 50 and self.elements.dial_right.angle <= 80 and
+	if self.elements.dial_right.angle >= 30 and self.elements.dial_right.angle <= 60 and
 		self.elements.light_3.on == false then
 		self.elements.light_3:toggle()
 	end
 
-	if (self.elements.dial_right.angle < 50 or self.elements.dial_right.angle > 80) and
+	if (self.elements.dial_right.angle < 30 or self.elements.dial_right.angle > 60) and
 		self.elements.light_3.on == true then
 		self.elements.light_3:toggle()
 	end
 
-	if math.abs(self.elements.dial_left.angle - self.elements.dial_right.angle) <= 45 and
+	if (math.abs(self.elements.dial_left.angle - self.elements.dial_right.angle) <= 30 or
+		math.abs(self.elements.dial_left.angle - self.elements.dial_right.angle) >= 330 ) and
 		self.elements.light_2.on == false then
 		self.elements.light_2:toggle()
 	end
 
-	-- bug: middle light does not light up when dials are on opposite sides of zero
-	-- could be an interesting bug to keep in
-	-- solution: <= 45 or > 315
-	-- tests would be useful here!
-	-- 30 might be sufficient, but it's nice it lights up for longer than the other two
 
-	if math.abs(self.elements.dial_left.angle - self.elements.dial_right.angle) > 45 and
+	if (math.abs(self.elements.dial_left.angle - self.elements.dial_right.angle) > 30 and
+		math.abs(self.elements.dial_left.angle - self.elements.dial_right.angle) < 330) and
 		self.elements.light_2.on == true then
 		self.elements.light_2:toggle()
 	end
